214d6f8f1c8f7d675f1fe59ba0a26be2fdb2dfda
askarakhmetov <askarahmetovufa@gmail.com>
2021-12-22 13:16:33 +0300

    Aggregator is at least working

--- app/src/main/scala/capstone/App.scala
+++ app/src/main/scala/capstone/App.scala
@@ -3,10 +3,14 @@
  */
 package capstone
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.expressions.Aggregator
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.types.{MapType, StringType}
+import org.apache.spark.sql.{DataFrame, Dataset, Encoder, Encoders, SparkSession, TypedColumn}
+
 import annotation.tailrec
 import scala.reflect.ClassTag
 import java.sql.Timestamp
@@ -57,10 +61,89 @@ object App {
     val projTable1 = spark.sql(query1.stripMargin)
     val projTable2 = spark.sql(query2.stripMargin)
 
-    projTable1.show(5, false)
-    projTable2.show(5,false)
+    projTable1.show(5, truncate = false)
+    projTable2.show(5, truncate = false)
 
-    spark.close()
+    import spark.implicits._
+    val dfds: Dataset[MobAppClickProj] =
+      df.withColumn("attributes",from_json(col("attributes"),MapType(StringType,StringType)))
+        .withColumn("eventTime", unix_timestamp(col("eventTime"), "yyyy-MM-dd HH:mm:ss")
+          .cast("timestamp"))
+        .as[MobAppClickProj]
+    dfds.show(5,truncate = false)
+
+    val extractingPurch: TypedColumn[MobAppClickProj, Set[String]] =
+      new Aggregator[MobAppClickProj, Set[String], Set[String]] with Serializable {
+        def zero: Set[String] = Set[String]()
+        def reduce(es: Set[String], macp: MobAppClickProj): Set[String] =
+          if (macp.attributes.getOrElse(None)!=None) {
+            val ttt = macp.attributes.get
+            if (ttt.contains("purchase_id")) {es+ttt("purchase_id")}
+            else es
+          } else es
+        def merge(wx: Set[String], wy: Set[String]): Set[String] = wx.union(wy)
+        def finish(columning: Set[String]): Set[String] = columning
+        def bufferEncoder: Encoder[Set[String]] = implicitly(ExpressionEncoder[Set[String]])
+        def outputEncoder: Encoder[Set[String]] = implicitly(ExpressionEncoder[Set[String]])
+      }.toColumn
+    val extractingChan: TypedColumn[MobAppClickProj, Set[String]] =
+      new Aggregator[MobAppClickProj, Set[String], Set[String]] with Serializable {
+        def zero: Set[String] = Set[String]()
+        def reduce(es: Set[String], macp: MobAppClickProj): Set[String] =
+          if (macp.attributes.getOrElse(None)!=None) {
+            val ttt = macp.attributes.get
+            if (ttt.contains("channel_id")) {es+ttt("channel_id")}
+            else es
+          } else es
+        def merge(wx: Set[String], wy: Set[String]): Set[String] = wx.union(wy)
+        def finish(columning: Set[String]): Set[String] = columning
+        def bufferEncoder: Encoder[Set[String]] = implicitly(ExpressionEncoder[Set[String]])
+        def outputEncoder: Encoder[Set[String]] = implicitly(ExpressionEncoder[Set[String]])
+      }.toColumn
+    val extractingCamp: TypedColumn[MobAppClickProj, Set[String]] =
+      new Aggregator[MobAppClickProj, Set[String], Set[String]] with Serializable {
+        def zero: Set[String] = Set[String]()
+        def reduce(es: Set[String], macp: MobAppClickProj): Set[String] =
+          if (macp.attributes.getOrElse(None)!=None) {
+            val ttt = macp.attributes.get
+            if (ttt.contains("campaign_id")) {es+ttt("campaign_id")}
+            else es
+          } else es
+        def merge(wx: Set[String], wy: Set[String]): Set[String] = wx.union(wy)
+        def finish(columning: Set[String]): Set[String] = columning
+        def bufferEncoder: Encoder[Set[String]] = implicitly(ExpressionEncoder[Set[String]])
+        def outputEncoder: Encoder[Set[String]] = implicitly(ExpressionEncoder[Set[String]])
+      }.toColumn
+
+    dfds.groupByKey(_.userId)
+      .agg(
+        // our aggregator functions at work
+        extractingPurch.name("purchaseId"),
+        extractingCamp.name("campaignId"),
+        extractingChan.name("channelId")
+      ).show(5)
+
   }
 
 }
+
+case class MobAppClickProj(userId: String,
+                           eventId: String,
+                           eventTime: Timestamp,
+                           eventType: String,
+                           attributes: Option[Map[String, String]])
+
+case class PurchasesProj(purchaseId: String,
+                         purchaseTime: Timestamp,
+                         billingCost: Double,
+                         isConfirmed: Boolean)
+
+case class PurchAttrProj(purchaseId: String,
+                         purchaseTime: Timestamp,
+                         billingCost: Double,
+                         isConfirmed: Boolean,
+                         // a session starts with app_open event and finishes with app_close
+                         sessionId: String,
+                         campaignId: String, // derived from app_open#attributes#campaign_id
+                         channelId: String // derived from app_open#attributes#channel_id
+                        )
